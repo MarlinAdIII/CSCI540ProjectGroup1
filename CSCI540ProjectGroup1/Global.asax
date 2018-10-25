c711c6a330c9f29b24eb49bf3ce3254351638169
Marlin Davis <madavis@EMAIL.USCUPSTATE.EDU>
2018-10-25 10:21:51 -0400

    Project Created

---
+++ CSCI540ProjectGroup1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CSCI540ProjectGroup1.MvcApplication" Language="C#" %>
---
+++ CSCI540ProjectGroup1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CSCI540ProjectGroup1.Startup))]
+namespace CSCI540ProjectGroup1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
